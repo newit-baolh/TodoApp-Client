1c80c1bc2a2f50e8efd472aa93f562cca0d1afd1
Hoai Bao <suzaku.vn@gmail.com>
2021-10-21 00:03:13 +0700

    edit button sort

--- src/components/Content/Control/Sort/Sort.js
+++ src/components/Content/Control/Sort/Sort.js
@@ -1,40 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
-Sort.propTypes = {
-    
-};
+Sort.propTypes = {};
 
 function Sort(props) {
     return (
-        <button
-            className="bg-blue-500 px-6 py-1.5 ml-20  rounded-md text-white border border-white hover:bg-blue-400 group-hover:block relative ">
-            Sắp xếp
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 inline-block pl-1"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+        <div className="bg-blue-500 ml-20 rounded-2xl ">
+            <select
+                className="select-primary block w-full px-7 py-1.5 outline-none  border border-blue-200 bg-blue-400 rounded rounded-md text-white"
             >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="{2}"
-                    d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
-                />
-            </svg>
-            <div
-                className="absolute top-10 left-0 w-full bg-blue-100 border border-black-500 rounded-md cursor-pointer hidden  hover:active ">
-                <div className="bg-blue-500 px-3 hover:bg-blue-400">Tên A-Z</div>
-                <div className="bg-blue-500 px-3 hover:bg-blue-400">Tên Z-A</div>
-                <hr/>
-                <div className="bg-blue-500 px-3 hover:bg-blue-400">Cần làm</div>
-                <div className="bg-blue-500 px-3 hover:bg-blue-400">Đang làm</div>
-                <div className="bg-blue-500 px-3 hover:bg-blue-400">Đã xong</div>
-            </div>
-        </button>
-    );
+                <option selected={true} disabled="disabled">Sắp xếp</option>
+                <option value="1">Tên A - Z</option>
+                <option value="2">Tên Z - A</option>
+                <option value="3">Cần làm</option>
+                <option value="4">Đang làm</option>
+                <option value="5">Đã xong</option>
+            </select>
+        </div>
+    )
 }
 
 export default Sort;
