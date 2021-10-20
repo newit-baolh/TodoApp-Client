13bbc0667184bfe21e4ad80d2b7abeceaec751f4
Hoai Bao <suzaku.vn@gmail.com>
2021-10-20 16:26:46 +0700

    add modelForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
-import TaskForm from "./components/TaskForm/TaskForm";
+import ModalForm from "./components/ModalForm/ModalForm";
 
 function App() {
     return (
         <div className="container mx-auto">
             <Header/>
             <div className="grid grid-cols-12 gap-x-10 m-4">
-                <TaskForm/>
+                <ModalForm/>
                 <Content/>
             </div>
         </div>
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import TaskForm from "../TaskForm/TaskForm";
 import Search from "./Control/Search/Search";
 import Sort from "./Control/Sort/Sort";
 import TaskList from "./TaskList/TaskList";
@@ -10,7 +8,7 @@ Content.propTypes = {};
 
 function Content(props) {
     return (
-        <div className="text-center col-end-13 col-span-8 rounded-lg shadow-lg border border-gray-200 ">
+        <div className="text-center col-end-13 col-span-12 rounded-lg shadow-lg border border-gray-200 ">
             <div className="text-left pb-2 ml-6 mt-5">
                 <button className="bg-blue-500 py-2 px-10 rounded-md text-white hover:bg-blue-400">
                     <i className="fas fa-plus-circle"/>&nbsp; Thêm mới
