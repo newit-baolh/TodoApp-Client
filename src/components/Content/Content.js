b2f5ca782662fa91ca8b1268cd0c7b8413d11d17
Hoai Bao <suzaku.vn@gmail.com>
2021-10-22 15:33:42 +0700

    add search filter

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -10,14 +10,22 @@ import {createTask, deleteTask, getList, updateTask} from "../../services/Servic
 function Content() {
     const [data, setData] = useState([])
     const [edit, setEdit] = useState(null)
+    const [filters, setFilters] = useState({
+        result: [],
+        keyword: ""
+    })
 
 
     useEffect(() => {
-        getList.then(res => setData(res)).catch(err => `Had problem ${err}`)
+        getList().then(res => {
+            setData(res)
+            setFilters({...filters,result: res})
+        }).catch(err => `Had problem ${err}`)
         return () => {
-            getList.then(res => setData(res)).catch(err => `Had problem ${err}`)
+            getList().then(res => setData(res)).catch(err => `Had problem ${err}`)
         }
     }, [])
+
 
     const onSubmitData = (value) => {
         if (value.data.id === "") {
@@ -60,13 +68,27 @@ function Content() {
         setEdit(null)
     }, [edit])
 
+    const onSearchItem = (value) => {
+        const keyword = value.keyword
+        if (value.search !== "") {
+            const filterData = filters.result.filter(item => {
+                let a = item.name.toLowerCase()
+                let b = keyword.toLowerCase()
+                return a.includes(b)
+            })
+            setData(filterData)
+
+        } else {
+            setData(filters.result)
+        }
+    }
     return (
         <div className="text-center col-end-13 col-span-12 rounded-lg shadow-lg border border-gray-200 ">
             <div className="text-left pb-2 ml-6 mt-5">
                 <ModalForm onSubmit={onSubmitData} edit={edit}/>
             </div>
             <div className="flex justify-start items-center mx-6 mt-4 relative mb-10">
-                <Search/>
+                <Search onSearch={onSearchItem}/>
                 <Sort/>
             </div>
             <div className="bg-white rounded-lg py-6">
--- src/services/Services.js
+++ src/services/Services.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const baseURL = "http://localhost:5000/api"
 
-export const getList = axios.get(`${baseURL}/list`).then(res=> res.data)
+export const getList = () => axios.get(`${baseURL}/list`).then(res => res.data)
 export const createTask = (data) => axios.post(`${baseURL}/add`, data)
-export const deleteTask = (item)=> axios.delete(`${baseURL}/delete/${item.id}`)
-export const updateTask = (item) => axios.put(`${baseURL}/update/${item.id}`,{name: item.name, status: item.status, description: item.description})
+export const deleteTask = (item) => axios.delete(`${baseURL}/delete/${item.id}`)
+export const updateTask = (item) => axios.put(`${baseURL}/update/${item.id}`, {
+    name: item.name,
+    status: item.status,
+    description: item.description
+})
